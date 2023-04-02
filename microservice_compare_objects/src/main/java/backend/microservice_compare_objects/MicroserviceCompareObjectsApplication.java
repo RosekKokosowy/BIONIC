0e61a9469c2a28b23b4fc3972cf39fbab69573dd
Mateusz Cybulski <235847@edu.p.lodz.pl>
2023-04-02 22:56:47 +0200

    switching project directories

---
+++ microservice_compare_objects/src/main/java/backend/microservice_compare_objects/MicroserviceCompareObjectsApplication.java
@@ -0,0 +1,13 @@
+package backend.microservice_compare_objects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCompareObjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceCompareObjectsApplication.class, args);
+    }
+
+}
---
+++ microservice_compare_objects/src/test/java/backend/microservice_compare_objects/MicroserviceCompareObjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.microservice_compare_objects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCompareObjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
