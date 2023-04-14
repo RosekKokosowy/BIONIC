7eaa3f533c478968a7f1b8f856d0efb2693fa99c
Mateusz Cybulski <235847@edu.p.lodz.pl>
2023-04-14 12:01:55 +0200

    Creating comparison controller

--- server/src/main/java/backend/server/web/ComparisonController.java
+++ server/src/main/java/backend/server/web/ComparisonController.java
@@ -1,17 +1,34 @@
 package backend.server.web;
 
+import backend.server.data.CarParameters;
+import backend.server.data.CarsInfo;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @RestController
 @RequestMapping("/car")
 public class ComparisonController {
 
-    @GetMapping("/compare")
-    public String compareCars(){
-        return "";
+    private final RestTemplate restTemplate;
+    private final String microservice_url = "http://localhost:8081/car/compare/comparison";
+
+    @Autowired
+    public ComparisonController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @PostMapping("/compare")
+    public ResponseEntity<CarParameters> compareCars(@RequestBody CarsInfo carsInfo){
+        CarParameters car = restTemplate.postForObject(
+                microservice_url,
+                carsInfo,
+                CarParameters.class
+        );
+        log.info(String.valueOf(car));
+        return ResponseEntity.ok(car);
     }
 }
